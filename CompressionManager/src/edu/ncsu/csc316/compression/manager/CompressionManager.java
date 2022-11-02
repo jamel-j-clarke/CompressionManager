09c7d8fc83b583641f967f68c2eff789d9f4e7e2
Jamel Clarke <jjclark4@ncsu.edu>
2022-11-01 23:37:23 -0400

    [Skeleton]
    Created a skeleton for the project.

---
+++ CompressionManager/src/edu/ncsu/csc316/compression/manager/CompressionManager.java
@@ -0,0 +1,32 @@
+package edu.ncsu.csc316.compression.manager;
+
+import java.io.FileNotFoundException;
+
+import edu.ncsu.csc316.dsa.list.List;
+import edu.ncsu.csc316.dsa.map.Map;
+
+
+/**
+ * 
+ * @author Jamel Clarke
+ * @author Xavier Jones
+ *
+ */
+public class CompressionManager {
+
+
+    public CompressionManager(String pathToInputFile) throws FileNotFoundException {
+        // TODO Complete this constructor
+    }
+
+    public Map<Integer, List<String>> getCompressed() {
+		return null;
+        // TODO Complete this method
+    }
+
+    public Map<Integer, List<String>> getDecompressed() {
+		return null;
+        // TODO Complete this method
+    }
+}
+
---
+++ CompressionManager/src/edu/ncsu/csc316/compression/manager/ReportManager.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package edu.ncsu.csc316.compression.manager;
+
+import java.io.FileNotFoundException;
+
+/**
+ * 
+ * @author Jamel Clarke
+ * @author Xavier Jones
+ *
+ */
+public class ReportManager {
+
+    private static final String INDENT = "   ";
+
+    public ReportManager(String pathToInputFile) throws FileNotFoundException {
+        // TODO Complete this constructor
+    }
+
+    public String compress() {
+		return null;
+        // TODO Complete this method
+    }
+    
+    public String decompress() {
+		return null;
+        // TODO Complete this method
+    }
+}
